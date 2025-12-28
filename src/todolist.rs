1aa2f35bf9e6180bbfb3320d5daeb2ff96889e18
Ciyss <bj856575@gmail.com>
2025-12-28 09:19:23 +0700

    menambah fungsi untuk menampilkan todo

--- src/todolist.rs
+++ src/todolist.rs
@@ -13,37 +13,67 @@ impl Todolist{
 fn add_todo(&mut self, input: String){
 
     // disini saya menggunakan perintah regex (r"\w+") untuk menemukan satu atau karakter lebih kata
-    self.todo = Regex::new(r"\w+")
+    let mut hasil: Vec<String> = Regex::new(r"\w+")
     .unwrap() 
     .find_iter(&input)
     .map(|x| x.as_str().to_string())
     .collect();
+
+    self.todo.append(&mut hasil);
 }
 
 fn remove_todo(&mut self, number: String){
-    // Parse indices from input string, ignore parse errors
-    let mut indices: Vec<usize> = number
+    // memparsing input string number dan mengabaikan error
+    let mut temp: Vec<usize> = number
         .split_ascii_whitespace()
         .filter_map(|s| s.parse::<usize>().ok())
         .collect();
 
-    // Remove from highest index to lowest so earlier removals don't shift later indices
-    indices.sort_unstable_by(|a, b| b.cmp(a));
+    // menghapus dari index yang terbesar ke terkecil sekaligus mengcompare
+    temp.sort_unstable_by(|a, b| b.cmp(a));
 
-    for idx in indices {
-        if idx < self.todo.len() {
+    for mut idx in temp {
+        if idx <= self.todo.len() {
+            idx -=1;
             self.todo.remove(idx);
+        } else {
+            println!("Penghapusan gagal")
         }
     }
 }
 
-fn displays(&self){
+fn done(&self){
+
+}
+
+fn list(&self){
         let mut num = 1;
     
         for value in &self.todo {
             println!("{}. {}", num, value);
             num+=1;
         }
+}
+
+fn display(&mut self, input: String){
+    if input.contains("todo"){
+        let replacement = input.replace("todo", " ");
+        if replacement.contains("add"){
+            let adds = replacement.replace("add", " ");
+            self.add_todo(adds);
+
+            println!("Add Succes");
+        } else if replacement.contains("done") {
+            let done = replacement.replace("done", " ");
+            self.remove_todo(done);
+
+            println!("Remove Succes");
+        } else {
+            self.list();
+        }
+    } else {
+        println!("Input tidak valid");
+    }
 }
 
 
@@ -55,10 +85,11 @@ fn displays(&self){
 #[test]
 fn dones() {
     let mut hasil = Todolist{
-        todo : vec!["abjad".to_string(), "Kanjut".to_string(), "alhasil".to_string()]
+        todo : vec!["abjad".to_string(), "Kanjut".to_string(), "alhasil".to_string(),"vupang".to_string()]
     };
 
-    hasil.remove_todo("1 ".to_string());
-    hasil.displays();
+    hasil.display("todo add halo sayadisini".to_string());
+    hasil.display("todo done 4".to_string());
+    hasil.display("todo".to_string());
 }
 
